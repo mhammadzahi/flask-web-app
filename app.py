41affcd9fbe4c9b11a6bdef9aa779ed1488c46fc
Mohamed Zahi <mohamedzahi33@gmail.com>
2024-02-20 18:32:46 +0400

    push.....,

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def home():
         
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     #app.run(port=8181, host="0.0.0.0")
-    app.run(debug=True)
+    #app.run(debug=True)
     #app.run()    pass
